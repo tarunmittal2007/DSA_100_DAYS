7cb418bea924a4daf65c42149a8a11b6acc78904
Tarun Mittal <tarunmittal8572@gmail.com>
2026-04-17 19:46:00 +0530

    Day 81: Add DSA problem 81

---
+++ Day_081.c
@@ -0,0 +1,52 @@
+/*Problem: Implement Bubble Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64
+*/
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        int swapped = 0;
+
+        for(int j = 0; j < n - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        if(!swapped)
+            break;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    bubbleSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
